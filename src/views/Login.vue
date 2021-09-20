4cfc9adaf8d243fe5e0e8f3a8df57f6bd011a422
Rodrigo Otávio Belo <rodrigootavio019@gmail.com>
2021-09-19 21:33:58 -0300

    login screen

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,28 @@
 <template>
-    
+    <div class="text-center">
+        <main class="form-signin">
+        <form @submit.prevent="this.submit()">
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" v-model="this.email">
+            <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" v-model="this.password">
+            <label for="floatingPassword">Password</label>
+            </div>
+
+            <!-- <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div> -->
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+        </form>
+        </main>        
+    </div>
 </template>
 
 <script lang="ts">
@@ -7,6 +30,55 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-    
+    data () {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        submit() {
+            this.$router.push('/');
+        }
+    }
 })
 </script>
+
+<style>
+
+div.text-center {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+
+.form-signin .checkbox {
+  font-weight: 400;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
